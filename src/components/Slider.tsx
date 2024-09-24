137e1f1ad4e80e4c12ac4c94c561dfb06e14b28d
ceydaulubas <ceyda.ulubas@gmail.com>
2024-09-24 23:57:13 +0300

    edit comments

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -21,21 +21,23 @@ const Slider: React.FC<SliderProps> = ({
   const totalSlides = children.length;
   const maxIndex = Math.ceil(totalSlides / visibleSlides) - 1;
 
-  // Kaydırma fonksiyonları
+  // Function to go to the next slide
   const goToNext = () => {
     setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, maxIndex));
   };
 
+  // Function to go to the previous slide
   const goToPrev = () => {
     setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
   };
 
+  // Function to go to a specific slide
   const goToSlide = (index: number) => {
     console.log('index', index)
     setCurrentIndex(index);
   };
 
-  // Görünür slaytların genişliği
+  // Width of visible slides
   const slideWidthPercentage = 100 / visibleSlides;
 
   return (
@@ -48,18 +50,18 @@ const Slider: React.FC<SliderProps> = ({
 
       <SlideTrack
         style={{
-          transform: `translateX(-${currentIndex * (100 / visibleSlides)}%)`,  // Her slayt grubunu kaydırmak için doğru hesaplama
+          transform: `translateX(-${currentIndex * (100 / visibleSlides)}%)`,  // Correct calculation for moving each slide group
           display: 'flex',
-          width: `100%`,
+          width: `100%`,  // Width of all slides
           transition: 'transform 0.3s ease-in-out',
         }}
       >
         {children.map((child, index) => (
           <Slide
             key={index}
-            visibleSlides={visibleSlides}  // Her slaytın genişliğini ayarlamak için
+            visibleSlides={visibleSlides}  // Set width for each slide
             style={{
-              flex: `0 0 ${slideWidthPercentage}%`,  // Her slaytın genişliği
+              flex: `0 0 ${slideWidthPercentage}%`,  // Width of each slide
               width: `${slideWidthPercentage}%`,
               boxSizing: 'border-box',
             }}
